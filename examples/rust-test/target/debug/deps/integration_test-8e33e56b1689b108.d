6fdc1dd6d0806c33657b9f3bbbd0db96c35d683d
Richard Cardone <richcar58@protonmail.com>
2026-02-15 11:30:41 -0600

    clean up test workflow

--- examples/rust-test/target/debug/deps/integration_test-8e33e56b1689b108.d
+++
@@ -1,5 +0,0 @@
-/home/rich/git/congo-rustgen/examples/rust-test/target/debug/deps/integration_test-8e33e56b1689b108.d: tests/integration_test.rs
-
-/home/rich/git/congo-rustgen/examples/rust-test/target/debug/deps/integration_test-8e33e56b1689b108: tests/integration_test.rs
-
-tests/integration_test.rs:
